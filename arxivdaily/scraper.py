96934a09fef674df831b13dea8379523fbcd9dad
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-03-03 15:56:32 -0500

    Get logger

--- arxivdaily/scraper.py
+++ arxivdaily/scraper.py
@@ -1,7 +1,14 @@
 import asyncio
+import logging
 from typing import Dict
 
 import aiohttp
+
+from . import config
+
+config.configure_logging()
+
+log = logging.getLogger(__name__)
 
 
 def pages() -> Dict[str, bytes]:
