95f87515e8960f69e7568bd3456bee8c9a296665
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-03-03 22:52:34 -0500

    Address mypy messages

--- arxivdaily/scraper.py
+++ arxivdaily/scraper.py
@@ -11,14 +11,15 @@ config.configure_logging()
 log = logging.getLogger(__name__)
 
 
-async def _get_pages() -> Dict[str, bytes]:
+async def _get_pages() -> Dict[str, str]:
     log.debug('Getting pages.')
+    tasks = {}
+    pages = {}
     # conn = aiohttp.TCPConnector(limit=config.MAX_CONNECTIONS)
     timeout = aiohttp.ClientTimeout(total=config.HTTP_TIMEOUT)
-    pages = {}
     async with aiohttp.ClientSession(timeout=timeout) as session:
 
-        async def get_page(url) -> bytes:
+        async def get_page(url: str) -> str:
             log.debug('Getting page for URL %s', url)
             async with session.get(url) as resp:
                 assert resp.status == 200
@@ -26,15 +27,15 @@ async def _get_pages() -> Dict[str, bytes]:
 
         for category in config.CATEGORIES:
             url = config.HTML_URL_TEMPLATE_MINIMAL.format(category=category)
-            pages[category] = asyncio.create_task(get_page(url))
+            tasks[category] = asyncio.create_task(get_page(url))
             log.debug('Created task for %s.', category)
         for category in config.CATEGORIES:
-            pages[category] = await pages[category]
+            pages[category] = await tasks[category]
             log.debug('Got page for %s having size %s.', category, len(pages[category]))
     return pages
 
 
-def get_pages() -> Dict[str, bytes]:
+def get_pages() -> Dict[str, str]:
     log.debug('Getting pages.')
     pages = asyncio.run(_get_pages(), debug=True)
     log.debug('Got pages.')
